55a76721f7d488d1427236bc3fe971f742973c85
veduruchandrashekar <102662246+veduruchandrashekar@users.noreply.github.com>
2022-05-12 10:41:30 +0100

    Update chandra.groovy

--- vars/chandra.groovy
+++ vars/chandra.groovy
@@ -1,7 +1,7 @@
 def call(tomcatIp,warName,credId){
   sshagent(["${cerdId}"]) {
-    sh "scp -o StrictHostKeyChecking=no target/webby.war ec2-user@${tomcatIp}:/opt/tomcat8/webapps/${warName}.war"
-            sh  "ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/startup.sh"
-            sh  "ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/shutdown.sh"
-}
+    sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@${tomcatIp}:/opt/tomcat8/webapps/${warName}.war"
+    sh  "ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/startup.sh"
+    sh  "ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/shutdown.sh"
+   }
 }
